470e1ae2c919ebeb9c0e1581b985d4b5c788e31f
cyborg-ubyvtsya <cyborg-ubyvtsya@users.noreply.github.com>
2021-04-12 19:46:25 +0300

    Changed spaces and added clipmenu hotkey

--- config.h
+++ config.h
@@ -67,6 +67,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-p", ">>>", NULL };
+static const char *clipmenu[] = { "clipmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", sel_bg, "-sf", sel_fg, "-p", "Clipboard >>>", NULL };
 static const char *termcmd[] = { "urxvt", NULL };
 static const char *cmuscmd[] = { "urxvt", "-e", "sh", "-c", "cmus", NULL };
 static const char *browcmd[] = { "firefox", NULL };
@@ -79,7 +80,8 @@ static const char *screens[] = { "scrot", "-s", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY|ShiftMask,             XK_c,      spawn,          {.v = clipmenu } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd  } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -89,14 +91,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,	                XK_w,      killclient,     {0} },
+	{ MODKEY,	                    XK_w,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,			XK_r,	   setlayout,	   {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,		XK_r,	   setlayout,	   {.v = &layouts[4]} },
-	{ MODKEY,			XK_c,	   setlayout,	   {.v = &layouts[5]} },
-	{ MODKEY,			XK_g,	   setlayout,	   {.v = &layouts[6]} },
+	{ MODKEY,		            	XK_r,	   setlayout,	   {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,		        XK_r,	   setlayout,	   {.v = &layouts[4]} },
+	{ MODKEY,	            		XK_c,	   setlayout,	   {.v = &layouts[5]} },
+	{ MODKEY,		            	XK_g,	   setlayout,	   {.v = &layouts[6]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -105,14 +107,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,			XK_q,	   setborderpx,    {.i = -1 } },
-	{ MODKEY,			XK_a,      setborderpx,    {.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_m,	   spawn,	   {.v = cmuscmd } },
-	{ MODKEY|ShiftMask,		XK_i,	   spawn,	   {.v = browcmd } },
-	{ MODKEY|ShiftMask,		XK_b,	   spawn,	   {.v = bitcoin } },
-	{ MODKEY|ShiftMask,		XK_k,	   spawn,	   {.v = keepass } },
-	{ MODKEY|ShiftMask,		XK_t,	   spawn,	   {.v = telegrm } },
-	{ MODKEY,			XK_Print,  spawn,	   {.v = screens } },
+	{ MODKEY,	            		XK_q,	   setborderpx,    {.i = -1 } },
+	{ MODKEY,		            	XK_a,      setborderpx,    {.i = +1 } },
+	{ MODKEY|ShiftMask,	            XK_m,	   spawn,          {.v = cmuscmd } },
+	{ MODKEY|ShiftMask,	            XK_i,	   spawn,          {.v = browcmd } },
+	{ MODKEY|ShiftMask,	            XK_b,	   spawn,          {.v = bitcoin } },
+	{ MODKEY|ShiftMask,	            XK_k,	   spawn,          {.v = keepass } },
+	{ MODKEY|ShiftMask,	            XK_t,	   spawn,          {.v = telegrm } },
+	{ MODKEY,                       XK_Print,  spawn,          {.v = screens } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
